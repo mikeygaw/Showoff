f75bdf4d93b967bca52b3f99b8909b8c4bcd6781
mikeygaw <mgawlinski16@gmail.com>
2016-06-07 16:02:00 -0400

    Update Calculate.java
    removed netbeans added code

--- Calculate.java
+++ Calculate.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mileskilometers;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Mike
- */
 public class Calculate {
     public static void milesToKilometers() {
         Scanner inpt = new Scanner(System.in);
