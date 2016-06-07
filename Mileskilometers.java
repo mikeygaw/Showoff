b13a30c644cd870b9817bf386692fa64eac2f18f
mikeygaw <mgawlinski16@gmail.com>
2016-06-07 16:00:11 -0400

    Update Mileskilometers.java
    removes netbeans added code

--- Mileskilometers.java
+++ Mileskilometers.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mileskilometers;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Mike
- */
 public class Mileskilometers {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Enter number to select option: ");
         System.out.println("1. Miles to kilometers");
